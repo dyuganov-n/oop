1e8e9a602a38982554844115eb2721520d0d2d59
dyuganov-n <dyuganov.n@gmail.com>
2020-10-26 17:13:12 +0700

    Memory leaks test added. Init of google tests.

--- lab1_rna/main.cpp
+++ lab1_rna/main.cpp
@@ -4,33 +4,46 @@
 
 #include "rna.h"
 
-#include <time.h>
+#include <time.h> // time
+
+// memory
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
 
 using namespace std;
 
 
 int main() {
+
+	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
+	_CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
+
+	_CrtMemState _ms;
+	_CrtMemCheckpoint(&_ms);
+
 	try {
-
-
-		// 1'000'000 nucls test
-		RNA r; 
-		clock_t tStart = clock();
-		for (size_t i = 0; i < 1'000'000; ++i) {
-			r.addNucl(Nucl::G);
+		_CrtDumpMemoryLeaks();
+		{
+			// 1'000'000 nucls test
+			RNA r;
+			clock_t tStart = clock();
+			for (size_t i = 0; i < 1'000'000; ++i) {
+				r.addNucl(Nucl::G);
+			}
+			printf("Time taken: %.2fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
+			cout << endl;
 		}
-		printf("Time taken: %.2fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
-		
+
 	}
 	catch (const int& e) {
 		if (e == 2) cout << endl << "Wrong RNA array index" << endl;
 		else cout << "Some error";
 		return -1;
 	}
+
+	_CrtMemDumpAllObjectsSince(&_ms);
+
 	return 0;
 }
 
-// TODO
-	// tests for every method
-	// memory leak check
-	// 1'000'000 time test
