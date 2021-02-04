b2c3fe97904b727e55cc6ff5c8ee27faffcf2d69
dyuganov-n <dyuganov.n@gmail.com>
2021-02-05 02:57:35 +0700

    StatCounter changes

--- lab5_morse_code/src/StatCounter.java
+++ lab5_morse_code/src/StatCounter.java
@@ -4,17 +4,34 @@ public class StatCounter {
     private Character symbol;
     private Integer counter = 0;
 
-    public StatCounter(Character symbol){ this.symbol = symbol; }
-
-    public Character getSymbol() { return symbol; }
-    public Integer getCounter() { return counter; }
-
-    public void increaseCnt() { ++counter; }
-    public void decrease() { --counter; }
-
-    public boolean equals(StatCounter other) {
-        return this.symbol.equals(other.symbol) && this.counter.equals(other.counter); // don't need counters compare?
+    public StatCounter(Character symbol){
+        this.symbol = symbol;
     }
 
-    public native int hashCode();
+    public Character getSymbol() {
+        return symbol;
+    }
+
+    public Integer getCounter() {
+        return counter;
+    }
+
+    public void increaseCnt() {
+        ++counter;
+    }
+
+    public void decrease() {
+        --counter;
+    }
+
+    public boolean equals(StatCounter other) {
+        if(other == null) return false;
+        return (this == other) || (this.symbol.equals(other.symbol) && this.counter.equals(other.counter));
+    }
+
+    //public native int hashCode();
+
+    public int hashCode() {
+        return (int)symbol;
+    }
 }
